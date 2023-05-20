cc4a0915a5505807ee20eca7afafdd35ce649957
David Nuon <davidnuongm@gmail.com>
2023-05-20 12:24:02 -0700

    Add curl

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    curl
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
