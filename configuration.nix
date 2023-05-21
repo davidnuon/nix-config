0df07a6adb516147ac98b4e96c68f60ff6c70c94
David Nuon <davidnuongm@gmail.com>
2023-05-21 00:03:09 -0700

    Add htop and tmux

--- configuration.nix
+++ configuration.nix
@@ -98,8 +98,10 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    git
     curl
+    tmux
+    htop
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
