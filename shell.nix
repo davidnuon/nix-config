3a455521c3c75f214f1552d5390f8b7c45804d2d
David Nuon <davidnuongm@gmail.com>
2023-05-23 16:46:40 -0700

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} } : {
+	default = pkgs.mkShell {
+		NIX_PATH = "/home/davidnuon/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=./configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
+		NIX_CONFIG = "extra-experimental-features = nix-command";
+		nativeBuildInputs = with pkgs; [
+			nix
+			home-manager
+			git
+		];
+	};
+} 
