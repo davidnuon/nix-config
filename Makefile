af13244a07c3161394db0e4a0c98301cc56b2c26
David Nuon <davidnuongm@gmail.com>
2023-05-24 01:18:06 -0700

    Add Makefile

--- Makefile
+++ Makefile
@@ -10,3 +10,8 @@ echo In Nix shell: ${IN_NIX_SHELL}
 lint: check-env
 	alejandra .
 
+nixos.build: check-env
+	sudo nixos-rebuild build -I nixos-config=./configuration.nix
+
+nixos.switch: check-env
+	sudo nixos-rebuild switch -I nixos-config=./configuration.nix
--- shell.nix
+++ shell.nix
@@ -1,13 +1,6 @@
 {pkgs ? import <nixpkgs> {}}: let
-  USER_HOME = builtins.getEnv "HOME";
 in {
   default = pkgs.mkShell {
-    NIX_PATH = pkgs.lib.strings.concatStringsSep ":" [
-      "${USER_HOME}/.nix-defexpr/channels"
-      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-      "nixos-config=./configuration.nix"
-      "/nix/var/nix/profiles/per-user/root/channels"
-    ];
     NIX_CONFIG = "extra-experimental-features = nix-command";
     nativeBuildInputs = with pkgs; [
       nix
