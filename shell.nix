ae68b29e396cbbae46a61e0e838d197c33b26ee7
David Nuon <davidnuongm@gmail.com>
2023-05-23 23:06:06 -0700

    Broke up path for NIX_PATH

--- shell.nix
+++ shell.nix
@@ -1,8 +1,19 @@
 {pkgs ? import <nixpkgs> {}}: let
   USER_HOME = builtins.getEnv "HOME";
+  joinStr = sep: acc: _list:
+    with pkgs.lib.lists; let
+      _left = foldr (a: b: a + sep + b) acc (init _list);
+      _right = last _list;
+    in
+      _left + _right;
 in {
   default = pkgs.mkShell {
-    NIX_PATH = "${USER_HOME}/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=./configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
+    NIX_PATH = joinStr ":" "" [
+      "${USER_HOME}/.nix-defexpr/channels"
+      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+      "nixos-config=./configuration.nix"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
     NIX_CONFIG = "extra-experimental-features = nix-command";
     nativeBuildInputs = with pkgs; [
       nix
