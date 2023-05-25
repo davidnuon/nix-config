27c7f63572474af52d238d35e680699c7e894a88
David Nuon <davidnuongm@gmail.com>
2023-05-24 22:59:22 -0700

    Use out of the box dark gnome theme

--- home/default.nix
+++ home/default.nix
@@ -21,11 +21,6 @@ with pkgs; {
       package = pkgs.papirus-icon-theme;
     };
 
-    theme = {
-      name = "palenight";
-      package = pkgs.palenight-theme;
-    };
-
     cursorTheme = {
       name = "Numix-Cursor";
       package = pkgs.numix-cursor-theme;
