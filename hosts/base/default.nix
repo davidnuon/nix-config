027d4a0f644989a8cc37db3679c7331cd189f332
David Nuon <davidnuongm@gmail.com>
2023-05-27 18:30:18 -0700

    Factored boot config into specific host

--- hosts/base/default.nix
+++ hosts/base/default.nix
@@ -10,11 +10,6 @@
     /etc/nixos/hardware-configuration.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
--- hosts/dn-jetbook.nix
+++ hosts/dn-jetbook.nix
@@ -9,5 +9,10 @@
     ./home
   ];
 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
   networking.hostName = "dn-jetbook";
 }
