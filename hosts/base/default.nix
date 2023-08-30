eb119cf5e6a6ffaf98ad4ab078b7736c1fe02dfc
David Nuon <davidnuongm@gmail.com>
2023-08-30 14:47:46 -0700

    Add flakes and nix-command

--- hosts/base/default.nix
+++ hosts/base/default.nix
@@ -16,6 +16,11 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Enable networking
   networking.networkmanager.enable = true;
 
