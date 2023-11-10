ce25a859fd5f0d801e3efba7f153acbcf03c06fc
David Nuon <davidnuongm@gmail.com>
2023-11-10 15:25:19 -0800

    Add virtualization

--- hosts/dn-silverbook.nix
+++ hosts/dn-silverbook.nix
@@ -7,6 +7,7 @@
   imports = [
     ./base
     ./home
+    ./virtualization
   ];
 
   # Bootloader.
@@ -18,4 +19,6 @@
   # Tailscale
   networking.firewall.checkReversePath = "loose";
   services.tailscale.enable = true;
+
+  services.flatpak.enable = true;
 }
