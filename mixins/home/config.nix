e6c9c91438ca71bef40c3bed91e84d03646bebfe
David Nuon <davidnuongm@gmail.com>
2024-01-29 10:16:53 -0800

    Brought out davidnuon home into a proper module

--- mixins/home/config.nix
+++ mixins/home/config.nix
@@ -1,59 +1,60 @@
 {pkgs, ...}:
 with pkgs; {
-  home.packages = [
-    discord
-    vscode
-    firefox
-  ];
+  home-manager.users.davidnuon = {
+    home.packages = [
+      discord
+      vscode
+      firefox
+    ];
 
-  programs.git = {
-    enable = true;
-    userName = "David Nuon";
-    userEmail = "davidnuongm@gmail.com";
-    extraConfig.core.editor = "vim";
-    extraConfig.init.defaultBranch = "main";
-    lfs.enable = true;
-  };
-
-  gtk = {
-    enable = true;
-
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+    programs.git = {
+      enable = true;
+      userName = "David Nuon";
+      userEmail = "davidnuongm@gmail.com";
+      extraConfig.core.editor = "vim";
+      extraConfig.init.defaultBranch = "main";
+      lfs.enable = true;
     };
 
-    cursorTheme = {
-      name = "Numix-Cursor";
-      package = pkgs.numix-cursor-theme;
-    };
+    gtk = {
+      enable = true;
 
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-  };
-
-  programs.gnome-terminal = {
-    themeVariant = "dark";
-    profile = {
-      colors = {
-        backgroundColor = "rgb(0, 43, 54)";
-        foregroundColor = "rgb(131, 148, 150)";
-        useThemeColors = false;
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
       };
-      visibleName = "default";
+
+      cursorTheme = {
+        name = "Numix-Cursor";
+        package = pkgs.numix-cursor-theme;
+      };
+
+      gtk3.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+
+      gtk4.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
     };
+
+    programs.gnome-terminal = {
+      themeVariant = "dark";
+      profile = {
+        colors = {
+          backgroundColor = "rgb(0, 43, 54)";
+          foregroundColor = "rgb(131, 148, 150)";
+          useThemeColors = false;
+        };
+        visibleName = "default";
+      };
+    };
+
+    home.sessionVariables.GTK_THEME = "palenight";
+    home.stateVersion = "22.11";
   };
-
-  home.sessionVariables.GTK_THEME = "palenight";
-
-  home.stateVersion = "22.11";
 }
