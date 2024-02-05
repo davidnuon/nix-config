4309e53f97cceda045bc7d045e464cb11e6f0443
David Nuon <davidnuongm@gmail.com>
2024-02-04 17:44:55 -0800

    Add virt-manager

--- mixins/virtualization/default.nix
+++ mixins/virtualization/default.nix
@@ -6,7 +6,10 @@
   imports = [];
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = ["davidnuon"];
-  #   virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.x11 = true;
+
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true; # virt-manager requires dconf to remember settings
+  environment.systemPackages = with pkgs; [virt-manager];
 }
