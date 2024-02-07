5365bfc241f678f94c80abc46168c21730a9e6a9
David Nuon <davidnuongm@gmail.com>
2024-02-06 23:20:57 -0800

    Change Makefile to use flakes

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   description = "davidnuon's NixOS configuration";
   inputs = {
-    home-manager-2211.url = "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
-    nixpkgs-2211.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixos-hardware.url = "https://github.com/NixOS/nixos-hardware/archive/83e571b.tar.gz";
 
+    home-manager-2211.url = "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
+    nixpkgs-2211.url = "github:NixOS/nixpkgs/nixos-22.11";
+
     home-manager-2311.url = "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
-    #home-manager-2311.url = "github:nix-community/home-manager/23.11.tar.gz";
     nixpkgs-2311.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
