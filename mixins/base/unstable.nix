141f9160e9dd2b6baa070b295eb4e2b959fe1a86
David Nuon <davidnuongm@gmail.com>
2024-02-08 02:16:05 -0800

    Add mscorefonts (#31)

--- mixins/base/unstable.nix
+++ mixins/base/unstable.nix
@@ -1,18 +1,16 @@
 {
-    config, 
-    pkgs,
-    specialArgs,
-    ...
-}: 
-    let 
-        pkgs = import specialArgs.nixpkgs-unstable {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-        };
-    in
-{
-    environment.systemPackages = with pkgs; [
-        google-chrome
-        vscode
-    ];
+  config,
+  pkgs,
+  specialArgs,
+  ...
+}: let
+  pkgs = import specialArgs.nixpkgs-unstable {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+  };
+in {
+  environment.systemPackages = with pkgs; [
+    google-chrome
+    vscode
+  ];
 }
