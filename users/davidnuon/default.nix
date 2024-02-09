c5143bad9106e76cc3c98631b63c2a99331912fd
David Nuon <davidnuongm@gmail.com>
2024-02-09 01:18:12 -0800

    user module refactor (#37)
    * Decompose home manager
    
    * Pulled out group changes into user module

--- users/davidnuon/default.nix
+++ users/davidnuon/default.nix
@@ -2,68 +2,22 @@
   pkgs,
   config,
   ...
-}:
-with pkgs; {
-  home-manager.users.davidnuon = {
-    home.packages = [
-      discord
+}: {
+  imports = [
+    ./home-manager
+  ];
+
+  users.users.davidnuon = {
+    isNormalUser = true;
+    description = "David Nuon";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+      "vboxusers"
+      "docker"
     ];
-
-    programs.git = {
-      enable = true;
-      userName = "David Nuon";
-      userEmail = "davidnuongm@gmail.com";
-      extraConfig.core.editor = "vim";
-      extraConfig.init.defaultBranch = "main";
-      lfs.enable = true;
-    };
-
-    gtk = {
-      enable = true;
-
-      iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.papirus-icon-theme;
-      };
-
-      cursorTheme = {
-        name = "Numix-Cursor";
-        package = pkgs.numix-cursor-theme;
-      };
-
-      gtk3.extraConfig = {
-        Settings = ''
-          gtk-application-prefer-dark-theme=1
-        '';
-      };
-
-      gtk4.extraConfig = {
-        Settings = ''
-          gtk-application-prefer-dark-theme=1
-        '';
-      };
-    };
-
-    programs.gnome-terminal = {
-      themeVariant = "dark";
-      profile = {
-        colors = {
-          backgroundColor = "rgb(0, 43, 54)";
-          foregroundColor = "rgb(131, 148, 150)";
-          useThemeColors = false;
-        };
-        visibleName = "default";
-      };
-    };
-
-    dconf.settings = {
-      "org/virt-manager/virt-manager/connections" = {
-        autoconnect = ["qemu:///system"];
-        uris = ["qemu:///system"];
-      };
-    };
-
-    # home.sessionVariables.GTK_THEME = "palenight";
-    home.stateVersion = stateVersion;
   };
+
+  home-manager.users.davidnuon.home.stateVersion = stateVersion;
 }
