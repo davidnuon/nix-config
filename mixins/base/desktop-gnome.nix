f548d0e34166e864399bfe7d828a6414786555a0
David Nuon <davidnuongm@gmail.com>
2024-03-15 10:55:03 -0700

    Add wayland back in

--- mixins/base/desktop-gnome.nix
+++ mixins/base/desktop-gnome.nix
@@ -4,7 +4,10 @@
   ...
 }: {
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+
   services.xserver.desktopManager.gnome.enable = true;
+
   programs.gnome-terminal.enable = true;
 
   services.xserver.displayManager.defaultSession = "gnome";
