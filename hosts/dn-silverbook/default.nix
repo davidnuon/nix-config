6af7e0e9403de52cb1f727c952c902a57e49638e
David Nuon <davidnuongm@gmail.com>
2024-03-22 10:54:21 -0700

    Add firmware update tools (#47)

--- hosts/dn-silverbook/default.nix
+++ hosts/dn-silverbook/default.nix
@@ -15,6 +15,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "dn-silverbook";
+  services.fwupd.enable = true;
 
   system.stateVersion = "23.11"; # Did you read the comment?
 }
