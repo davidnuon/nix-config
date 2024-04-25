918a9115fddeb7c014ea4952ad50c095acfe64c2
David Nuon <davidnuongm@gmail.com>
2024-04-24 21:22:42 -0700

    Disable network wait manager

--- hosts/dn-grill/default.nix
+++ hosts/dn-grill/default.nix
@@ -21,14 +21,18 @@
 
   # enp0s13f0u2 is the Framework Ethernet Module
   networking.interfaces.enp0s13f0u2.useDHCP = true;
-  networking.interfaces.br0.useDHCP = true;
 
   # Bridge network so VMs can be exposed to the network
-  networking.bridges = {
-    "br0" = {
-      interfaces = ["enp0s13f0u2"];
-    };
-  };
+  # networking.bridges = {
+  #   "bridge0" = {
+  #     interfaces = ["enp0s13f0u2"];
+  #   };
+  # };
+  # networking.interfaces.bridge0.useDHCP = true;
+
+
+  systemd.network.wait-online.enable = false;
+  boot.initrd.systemd.network.wait-online.enable = false;
 
   # This isn't going anywhere
   time.timeZone = "America/Los_Angeles";
--- mixins/virtualization/default.nix
+++ mixins/virtualization/default.nix
@@ -4,9 +4,9 @@
   ...
 }: {
   imports = [];
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;
+  # virtualisation.virtualbox.host.enable = true;
+  # virtualisation.virtualbox.guest.enable = true;
+  # virtualisation.virtualbox.guest.x11 = true;
 
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true; # virt-manager requires dconf to remember settings
