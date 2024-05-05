cc5bc742c124d34c8d5971465a9db79c0600dfdc
David Nuon <davidnuongm@gmail.com>
2024-05-05 03:31:47 -0700

    Added a bunch of programs

--- mixins/hyprland/default.nix
+++ mixins/hyprland/default.nix
@@ -4,8 +4,59 @@
   config,
   ...
 }: {
-  programs.hyprland.enable = true;
+  programs = {
+    hyprland = {
+      enable = true;
+    };
+    waybar = {
+      enable = true;
+      package = pkgs.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+      });
+    };
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+  };
+
   environment.systemPackages = with pkgs; [
-	kitty
+    kitty
+    mako
+
+    polkit_gnome
+    libva-utils
+    fuseiso
+    udiskie
+    gnome.adwaita-icon-theme
+    gnome.gnome-themes-extra
+    gsettings-desktop-schemas
+    swaynotificationcenter
+    wlr-randr
+    ydotool
+    wl-clipboard
+    hyprland-protocols
+    hyprpicker
+    # swayidle
+    # swaylock
+    xdg-desktop-portal-hyprland
+    hyprpaper
+    wofi
+    firefox-wayland
+    swww
+    grim
+    xdg-utils
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    qt5.qtwayland
+    qt6.qmake
+    qt6.qtwayland
+    adwaita-qt
+    adwaita-qt6
+
+    pavucontrol
   ];
 }
