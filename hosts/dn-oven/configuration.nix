03a433faa4360f1b691bacf29e8286dc8d743953
David Nuon <davidnuongm@gmail.com>
2025-03-26 20:20:49 +0000

    Add dn-oven

---
+++ hosts/dn-oven/configuration.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "dn-oven";
+
+  system.stateVersion = "24.11";
+}
