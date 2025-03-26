5479201bca60633364fd7ae059cf088949db34cc
David Nuon <davidnuongm@gmail.com>
2025-03-26 13:27:59 -0700

    Feature/dn oven (#59)
    * Add dn-oven
    
    * No sleeping

---
+++ hosts/dn-oven/configuration.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "dn-oven";
+
+  # Disable sleepy behavior
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
+  system.stateVersion = "24.11";
+}
