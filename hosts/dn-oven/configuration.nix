c0afdd337e8eceb3004421aef2bd05e74f4f3dd1
David Nuon <davidnuongm@gmail.com>
2025-03-26 20:25:59 +0000

    No sleeping

--- hosts/dn-oven/configuration.nix
+++ hosts/dn-oven/configuration.nix
@@ -14,5 +14,11 @@
 
   networking.hostName = "dn-oven";
 
+  # Disable sleepy behavior
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   system.stateVersion = "24.11";
 }
