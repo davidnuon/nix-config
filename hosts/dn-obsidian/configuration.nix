1566bfd49f0a418d3e2d7e7ba094b263596dc7c6
David Nuon <davidnuongm@gmail.com>
2025-03-26 23:44:27 -0700

    Start on dn-obsidian

---
+++ hosts/dn-obsidian/configuration.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
