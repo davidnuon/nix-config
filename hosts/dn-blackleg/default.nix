c3f08f234d2d411560eba6dbd5bbb564d76e9aba
David Nuon <davidnuongm@gmail.com>
2025-07-18 20:30:33 -0700

    Started flake upgrade

--- hosts/dn-blackleg/default.nix
+++ hosts/dn-blackleg/default.nix
@@ -1,12 +1,12 @@
 {specialArgs, ...}:
-specialArgs.nixpkgs-2411.lib.nixosSystem {
+specialArgs.nixpkgs-2505.lib.nixosSystem {
   inherit specialArgs;
   system = "aarch64-linux";
   modules = [
     specialArgs.nixos-x13s.nixosModules.default
 
-    (import "${specialArgs.home-manager-2411}/nixos")
-    (import ../../users/davidnuon {stateVersion = "24.11";})
+    (import "${specialArgs.home-manager-2505}/nixos")
+    (import ../../users/davidnuon {stateVersion = "25.05";})
 
     ../../mixins/base
     ../../mixins/docker
