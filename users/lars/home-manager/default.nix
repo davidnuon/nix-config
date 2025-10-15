d0b4b6aa9e4f1e869f06ced815f50b122e7efab6
David Nuon <davidnuongm@gmail.com>
2025-10-14 18:01:14 -0700

    [dn-clapper] Add config for dn-clapepr

---
+++ users/lars/home-manager/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    ./packages.nix
+    ./git.nix
+    ./gnome.nix
+    ./gtk.nix
+  ];
+
+  home-manager.users.davidnuon.dconf.settings = lib.mkIf (config.virtualisation.libvirtd.enable) {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
+    };
+  };
+}
