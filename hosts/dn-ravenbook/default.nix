490ada38054638bffaee1e10ca4a65e6682ee982
David Nuon <davidnuongm@gmail.com>
2025-12-06 16:43:16 -0800

    Chore/upgrade dn ravenbook (#74)
    * [dn-ravenbook] Update flake and dn-ravenbook
    
    * [dn-ravenbook] Update hardware
    
    * [dn-ravenbook] Add KDE

--- hosts/dn-ravenbook/default.nix
+++ hosts/dn-ravenbook/default.nix
@@ -1,20 +1,18 @@
 {specialArgs, ...}:
-specialArgs.nixpkgs-2405.lib.nixosSystem {
+specialArgs.nixpkgs-2505.lib.nixosSystem {
   inherit specialArgs;
   system = "x86_64-linux";
   modules = [
     ./configuration.nix
     ./hardware-configuration.nix
 
-    (import "${specialArgs.home-manager-2405}/nixos")
-    (import ../../users/davidnuon {stateVersion = "24.05";})
-
-    "${specialArgs.nixos-hardware}/framework/13-inch/7040-amd"
+    (import "${specialArgs.home-manager-2505}/nixos")
+    (import ../../users/davidnuon {stateVersion = "25.05";})
 
     ../../mixins/base
     ../../mixins/docker
     ../../mixins/tailscale
     ../../mixins/flatpak
-    ../../mixins/godot
+    ../../mixins/kde
   ];
 }
