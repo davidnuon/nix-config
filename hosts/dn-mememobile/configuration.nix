3ffa201131b6df7cb4df8e8e0b0ef4b9a1c53638
David Nuon <davidnuongm@gmail.com>
2025-12-31 20:52:40 -0800

    Add dn-mememobile (#77)

---
+++ hosts/dn-mememobile/configuration.nix
@@ -0,0 +1,23 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.openssh.enable = true;
+  services.avahi.enable = true;
+
+  networking.hostName = "dn-mememobile";
+
+  programs.ssh.forwardX11 = true;
+  services.openssh.settings.X11Forwarding = true;
+
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
