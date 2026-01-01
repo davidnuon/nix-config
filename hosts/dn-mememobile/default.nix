507d21e0b3cc5108567122ab5a05565ce77727ed
David Nuon <davidnuongm@gmail.com>
2025-12-31 20:43:00 -0800

    Add dn-mememobile

---
+++ hosts/dn-mememobile/default.nix
@@ -0,0 +1,25 @@
+{specialArgs, ...}:
+specialArgs.nixpkgs-2511.lib.nixosSystem {
+  inherit specialArgs;
+  system = "x86_64-linux";
+  modules = [
+    ./configuration.nix
+    ./hardware-configuration.nix
+    ./nvidia.nix
+
+    (import "${specialArgs.home-manager-2511}/nixos")
+    (import ../../users/davidnuon {stateVersion = "25.11";})
+
+    ../../mixins/base
+    ../../mixins/docker
+    ../../mixins/tailscale
+    ../../mixins/flatpak
+    ../../mixins/xivlauncher
+    ../../mixins/steam
+    ../../mixins/kde
+    ../../mixins/input-remapper
+    ../../mixins/distrobox
+    ../../mixins/forge-mtg
+    ../../mixins/virtualization 
+  ];
+}
