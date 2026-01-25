afb893fca10bd61114ccb61b371947a6d3967a0c
David Nuon <davidnuongm@gmail.com>
2026-01-25 01:16:40 -0800

    Add ollama port

--- hosts/dn-mememobile/ollama.nix
+++ hosts/dn-mememobile/ollama.nix
@@ -4,10 +4,12 @@
   pkgs,
   ...
 }: {
+  networking.firewall.allowedTCPPorts = [11434];
   services.ollama = {
     enable = true;
     # Optional: preload models, see https://ollama.com/library
     loadModels = ["llama3.2:3b" "deepseek-r1:1.5b" "gpt-oss"];
     acceleration = "vulkan";
+    host = "0.0.0.0";
   };
 }
