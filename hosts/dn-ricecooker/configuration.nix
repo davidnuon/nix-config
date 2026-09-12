2b9172e8cef9e4f7b7be47528171d6dfd19b4a99
David Nuon <davidnuongm@gmail.com>
2026-09-12 02:34:55 -0700

    Disable wifi for dn-ricecooker (#107)

--- hosts/dn-ricecooker/configuration.nix
+++ hosts/dn-ricecooker/configuration.nix
@@ -26,6 +26,9 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+ 
+
+  networking.interfaces.wlp2s0.useDHCP = false;
 
   system.stateVersion = "25.11";
 }
