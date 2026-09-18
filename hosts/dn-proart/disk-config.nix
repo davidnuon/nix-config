5f6683248fb2ba7f3f44881971043fc1efe91fb9
David Nuon <davidnuongm@gmail.com>
2026-09-18 07:59:41 +0000

    Update disk config

--- hosts/dn-proart/disk-config.nix
+++ hosts/dn-proart/disk-config.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       my-disk = {
-        device = "/dev/sda";
+        device = "/dev/nvme0n1";
         type = "disk";
         content = {
           type = "gpt";
