c37e277834d39927ea48195d7ab30426f85417a3
David Nuon <davidnuongm@gmail.com>
2026-09-18 00:39:22 -0700

    commit progress
    dunno why keyboard doens't work

--- hosts/dn-proart/configuration.nix
+++ hosts/dn-proart/configuration.nix
@@ -7,9 +7,16 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.consoleMode = "0"; # or "0", "1", "2", "auto"
 
   networking.hostName = "dn-proart";
   services.fwupd.enable = true;
+
+  console = {
+    earlySetup = true;
+    packages = with pkgs; [terminus_font];
+    font = "ter-v32n"; # or "ter-u28n" for a slightly smaller large option
+  };
 
   system.stateVersion = "26.05"; # Did you read the comment?
 }
